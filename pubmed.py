63c90518905c41ae6e8661884f7c4da2fe1f9e27
the-brainstorming-guy <124350933+the-brainstorming-guy@users.noreply.github.com>
2024-11-20 21:35:40 +0100

    Create pubmed.py

---
+++ pubmed.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://pubmed.ncbi.nlm.nih.gov/33221700/"
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
+    "DNT": "1",  # Do Not Track request
+    "Upgrade-Insecure-Requests": "1",
+    "Cache-Control": "no-cache",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1"
+}
+
+r = requests.get(url, headers=headers)
+soup = BeautifulSoup(r.text, "html.parser")
+print(soup.select("h1"))
